203d5f3cd5c10865e6dbe1db3fdded0b2b8ce606
Marco Ripa <marcoripa@hotmail.it>
2022-05-26 09:03:49 +0200

    event01

--- src/Counter.js
+++ src/Counter.js
@@ -1,37 +1,24 @@
 import React from "react";
 
-class CounterDisplay extends React.Component {
-    render() {
-        return <h1>{this.props.counter}</h1>
-    }
-}
-
-export class Counter extends React.Component{
+export class ClickCounter extends React.Component {
     state = {
-        count: this.props.initialValue,
-        interval: this.props.intervalValue,
-        increment: this.props.incrementValue
+        count: 0
     }
     
-    // constructor(props) {
-    //     super(props)
-    // }
-    
+    handleIncrement = () => {
+        this.setState(state => {
+            return {
+                count: state.count + 1
+            }
+        })
+    }
 
     render() {
         return (
-            <CounterDisplay counter = {this.state.count}/>
+            <div>
+               {this.state.count}
+               <button onClick = {this.handleIncrement}>Increment</button>
+            </div>           
         )
-    }
-
-    componentDidMount() {
-
-        setInterval(() => {
-            this.setState((state) => {
-                return {
-                    count: state.count + state.increment
-                }
-            })
-        }, this.state.interval);
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {Counter} from "./Counter"
+import {ClickCounter} from "./Counter"
 
 
-ReactDOM.render(<Counter initialValue = {50} intervalValue = {500} incrementValue = {5}/>, document.getElementById('root'))
+ReactDOM.render(<ClickCounter />, document.getElementById('root'))
