7b8a60007dba3192a65237dded36272255862cdf
Marco Ripa <marcoripa@hotmail.it>
2022-05-30 17:51:08 +0200

    list06

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -15,27 +15,36 @@ export class TodoList extends React.Component {
             addItem: ''
         })
     } 
+
     handleInput = (event) => {
         this.setState({
             addItem: event.target.value
         })
     }
+
     handleReset = () => {
         this.setState({
             items: []
         })
     }
 
+    handleRemove = (event) => {
+        event.target.parentElement.remove()
+    }
+
     render() {
         return (
             <div className='container'>
-                {this.state.items.map(item => {
-                    return (
-                        <ul>
-                            <li>{item}</li>
+                      <ul>
+                    {this.state.items.map((item, index) => (
+                        <li key={index}>{item}
+                               <button 
+                                onClick={this.handleRemove} className='remove'>remove
+                            </button>   
+                        </li>       
+                        )
+                        )}
                       </ul>
-                  )
-                })}
                 <input
                     value={this.state.addItem}
                     onChange={this.handleInput}></input>
