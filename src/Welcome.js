0884f8f6c7764a0119e016952f7317e4f4edd37c
Marco Ripa <marcoripa@hotmail.it>
2022-06-03 09:57:49 +0200

    function01

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,11 +1,5 @@
-import React from "react";
-
-export class Welcome extends React.Component {
-    render() {
-        return (
-            <div className="welcome">
-                <p>Hello {this.props.name}!</p>
-            </div>
-        )
-    }
+function Welcome({name}) {
+    return <p>Hello {name}!</p>
 }
+
+export default Welcome
