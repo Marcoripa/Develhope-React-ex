8a6fb2db17429c7e77d28f7d174e1cd792286643
Marco Ripa <marcoripa@hotmail.it>
2022-06-07 09:42:30 +0200

    hooks04

--- src/useGitHubUser.jsx
+++ src/useGitHubUser.jsx
@@ -2,11 +2,27 @@ import { useEffect, useState } from "react";
 
 export function useFetch(url) {
     const [user, setUser] = useState('')
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
+
+    async function getUser() {
+        setLoading(true)
+        setError(null)
+
+        try {
+            const res = await fetch(url)
+            const json = await res.json()
+            setUser(json)
+        }
+
+        catch (error) {
+            setError(error)
+            setUser(null)
+        }
+    }
 
     useEffect(() => {
-        fetch(url)
-        .then(res => res.json())
-        .then(data => setUser(data))
+        getUser()
     }, [url])
 
     return {user}
