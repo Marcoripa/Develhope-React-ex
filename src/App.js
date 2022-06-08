3befb88dc95b11ac8b27f465a1ad153ad25fb2d4
Marco Ripa <marcoripa@hotmail.it>
2022-06-08 19:19:59 +0200

    route04

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import Welcome from "./Welcome";
 import {Counter} from './Counter'
 import { ShowGitHubUser } from "./ShowGitHubUser";
@@ -8,6 +8,11 @@ import { ShowGitHubUser } from "./ShowGitHubUser";
 export function App() {
     return (
         <>
+        <nav>
+            <Link to='/'>Home</Link>
+            <Link to='/counter'>Counter</Link>
+            <Link to='./:username'>Tony</Link>
+        </nav>
          <Routes>
             <Route path='/' element={<Welcome name='Tony'/>}/>
             <Route path='/counter' element={<Counter />} />
