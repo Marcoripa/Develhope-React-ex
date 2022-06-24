8d034394904b6d546f4e2cecc96f06f13096ee5f
Marco Ripa <marcoripa@hotmail.it>
2022-06-24 14:35:51 +0200

    redux05

--- src/index.js
+++ src/index.js
@@ -1,25 +1,33 @@
-import { incrementCounter } from './Redux/CounterReducer'
+import { incrementCounter, decrementCounter } from './Redux/CounterReducer'
 import {store} from './Redux/Store'
 import {addTodo, deleteTodo, editTodo} from './Redux/TodosReducer'
 
 //initial state
-console.log(store.getState())
+store.subscribe(() => {
+    console.log(store.getState())
+})
 
 //add an item
 store.dispatch(addTodo(3, 'Bake a cake', false))
-console.log(store.getState())
+
 
 //increment counter
 store.dispatch(incrementCounter())
-console.log(store.getState())
+
 
 //delete an item
 store.dispatch(deleteTodo(1))
-console.log(store.getState())
+
+//increment counter
+store.dispatch(incrementCounter())
+
+//decrement counter
+store.dispatch(decrementCounter())
+
 
 //edit an item
 store.dispatch(editTodo(2, 'New Title', true))
-console.log(store.getState())
+
 
 
 
